dbe469f670a375cbfac2635815811e2c52112289
Evan Kreutzwiser <53940800+Evan-Kreutzwiser@users.noreply.github.com>
2026-06-13 20:57:12 -0400

    apply water physics, splash sound, etc. to arcane fade

--- src/main/java/io/wispforest/affinity/mixin/EntityMixin.java
+++ src/main/java/io/wispforest/affinity/mixin/EntityMixin.java
@@ -1,5 +1,7 @@
 package io.wispforest.affinity.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import io.wispforest.affinity.Affinity;
@@ -8,13 +10,19 @@ import io.wispforest.affinity.item.ArtifactBladeItem;
 import io.wispforest.affinity.misc.ArcaneFadeFluid;
 import io.wispforest.affinity.misc.EntityReference;
 import io.wispforest.affinity.misc.quack.AffinityEntityAddon;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.MovementType;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.FluidState;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -30,10 +38,18 @@ import java.util.Map;
 public abstract class EntityMixin implements AffinityEntityAddon {
 
     @Shadow
-    public abstract boolean updateMovementInFluid(TagKey<Fluid> tag, double speed);
+    public abstract Box getBoundingBox();
 
     @Shadow
-    protected boolean touchingWater;
+    public abstract boolean isRegionUnloaded();
+
+    @Shadow
+    private World world;
+
+    @Shadow
+    public abstract void setVelocity(Vec3d velocity);
+
+    @Unique private static final boolean SINYTRA_CONNECTOR = FabricLoader.getInstance().isModLoaded("connector");
 
     @Unique
     private static final TagKey<Fluid> ARCANE_FADE = TagKey.of(RegistryKeys.FLUID, Affinity.id("arcane_fade"));
@@ -92,16 +108,63 @@ public abstract class EntityMixin implements AffinityEntityAddon {
         cir.setReturnValue(cir.getReturnValueF() * 2.5f);
     }
 
-    @Inject(method = "updateWaterState", at = @At("RETURN"), cancellable = true)
-    protected void updateFadeState(CallbackInfoReturnable<Boolean> cir) {
+    // Replicate vanilla fluid collision mechanic for use under unreliable sinytra connector environments
+    @Unique
+    public boolean affinity$vanillaUpdateFluidHeight(TagKey<Fluid> fluidTag) {
+        if (this.isRegionUnloaded()) {
+            return false;
+        } else {
+            Box aabb = this.getBoundingBox().contract(0.001);
+            int i = MathHelper.floor(aabb.minX);
+            int j = MathHelper.ceil(aabb.maxX);
+            int k = MathHelper.floor(aabb.minY);
+            int l = MathHelper.ceil(aabb.maxY);
+            int i1 = MathHelper.floor(aabb.minZ);
+            int j1 = MathHelper.ceil(aabb.maxZ);
+            BlockPos.Mutable pos = new BlockPos.Mutable();
+
+            for (int l1 = i; l1 < j; l1++) {
+                for (int i2 = k; i2 < l; i2++) {
+                    for (int j2 = i1; j2 < j1; j2++) {
+                        pos.set(l1, i2, j2);
+                        FluidState fluidstate = this.world.getFluidState(pos);
+                        if (fluidstate.isIn(fluidTag)) {
+                            double d1 = (float)i2 + fluidstate.getHeight(this.world, pos);
+                            if (d1 >= aabb.minY) {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    @WrapOperation(
+            method = "checkWaterState",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/entity/Entity;updateMovementInFluid(Lnet/minecraft/registry/tag/TagKey;D)Z"
+            )
+    )
+    private boolean checkFadeState(Entity instance, TagKey<Fluid> tag, double speed, Operation<Boolean> original) {
+        boolean vanillaWater = original.call(instance, tag, speed);
         boolean wasTouchingFade = this.affinity$touchingBleach;
-        this.touchingWater |= this.affinity$touchingBleach = this.updateMovementInFluid(ARCANE_FADE, 0.014);
+        if (!SINYTRA_CONNECTOR) {
+            this.affinity$touchingBleach = instance.updateMovementInFluid(ARCANE_FADE, 0.014);
+        } else {
+            // Under neoforge with sinytra connector, updateMovementInFluid doesn't work with tags
+            // and we have to duplicate the collision logic ourselves.
+            this.affinity$touchingBleach = affinity$vanillaUpdateFluidHeight(ARCANE_FADE);
+        }
 
         if (this.affinity$touchingBleach && !wasTouchingFade) {
             ArcaneFadeFluid.ENTITY_TOUCH_EVENT.invoker().onTouch((Entity) (Object) this);
         }
 
-        cir.setReturnValue(this.touchingWater || cir.getReturnValue());
+        return vanillaWater || this.affinity$touchingBleach;
     }
 
     @Inject(method = "tick", at = @At("TAIL"))
